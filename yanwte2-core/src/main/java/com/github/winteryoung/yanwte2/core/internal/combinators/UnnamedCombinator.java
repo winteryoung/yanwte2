63f252b28fc2ace9025e2fc644f36ad9a7bd0fff
Winter Young <513805252@qq.com>
2017-12-17 16:54:10 +0800

    test hook

--- yanwte2-core/src/main/java/com/github/winteryoung/yanwte2/core/internal/combinators/UnnamedCombinator.java
+++ yanwte2-core/src/main/java/com/github/winteryoung/yanwte2/core/internal/combinators/UnnamedCombinator.java
@@ -8,6 +8,7 @@ import com.github.winteryoung.yanwte2.core.internal.ServiceProviderLocators;
 import com.github.winteryoung.yanwte2.core.spi.Combinator;
 import com.github.winteryoung.yanwte2.core.spi.ServiceProviderLocator;
 
+
 import com.github.winteryoung.yanwte2.core.spi.SurrogateCombinator;
 import com.github.winteryoung.yanwte2.core.utils.Lazy;
 import com.google.common.collect.Sets;
